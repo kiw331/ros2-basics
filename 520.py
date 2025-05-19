3abcd29b5b33c81ae8c335ba21d218676da80325
Inwoo Kang <mingureion@gmail.com>
2025-05-20 03:12:51 +0900

    Update 520.py

--- 520.py
+++ 520.py
@@ -10,14 +10,14 @@ class Shelf:
 
     def build_stack(self):
         self.stack = []
-        for i in range(self.row):       # 행: x 증가
-            for j in range(self.col):   # 열: y 감소
+        for i in range(self.row):
+            for j in range(self.col):
                 x = self.first[0] + i * self.interval
-                y = self.first[1] - j * self.interval
+                y = self.first[1] - j * self.interval  # 열은 y 감소
                 p = posx(x, y, self.first[2], self.first[3], self.first[4], self.first[5])
                 self.stack.append(p)
 
-    def get_cup_cord(self):
+    def get_last_cord(self):
         if self.cnt == 0:
             tp_log("경고: 아이템이 없습니다.")
             return None
@@ -35,16 +35,26 @@ class Shelf:
             return
         self.cnt -= 1
 
-
 class CupShelf(Shelf):
     def __init__(self, cnt=0, first=posx(0,0,0,0,0,0), interval=0):
         super().__init__(cnt, first, interval, row=2, col=3)
+
+    def get_cup_cord(self):
+        return self.get_last_cord()
+
 
 class IceShelf(Shelf):
     def __init__(self, cnt=0, first=posx(0,0,0,0,0,0), interval=0):
         super().__init__(cnt, first, interval, row=3, col=3)
 
+    def get_ice_cord(self):
+        return self.get_last_cord()
+
+
 class MilkShelf(Shelf):
     def __init__(self, cnt=0, first=posx(0,0,0,0,0,0), interval=0):
         super().__init__(cnt, first, interval, row=1, col=3)
+
+    def get_milk_cord(self):
+        return self.get_last_cord()
 
